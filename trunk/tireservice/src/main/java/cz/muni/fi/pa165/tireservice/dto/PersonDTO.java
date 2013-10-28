bf47d79da91ebeab18f4c270d0e8a102c1e88fa0
martinmakarsky@gmail.com <martinmakarsky@gmail.com@fdf17263-572c-81d4-9246-cce687cf71e3>
2013-10-28 17:13:50 +0000

    Added almost final PersonServices.java, created PersonDTOImplTests.java without Tests

---
+++ trunk/tireservice/src/main/java/cz/muni/fi/pa165/tireservice/dto/PersonDTO.java
@@ -0,0 +1,158 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pa165.tireservice.dto;
+
+import cz.muni.fi.pa165.tireservice.entities.Order;
+import java.util.List;
+
+/**
+ *
+ * @author Martin
+ */
+public class PersonDTO {
+
+    private Long id;
+    
+
+    private List<Order> orders;
+    
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+    private String password; //Password type will change later in the semester.
+    
+    private boolean active;
+    private boolean isServiceman;
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public boolean isIsServiceman() {
+        return isServiceman;
+    }
+
+    public void setIsServiceman(boolean isServiceman) {
+        this.isServiceman = isServiceman;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + (this.id != null ? this.id.hashCode() : 0);
+        hash = 97 * hash + (this.orders != null ? this.orders.hashCode() : 0);
+        hash = 97 * hash + (this.firstName != null ? this.firstName.hashCode() : 0);
+        hash = 97 * hash + (this.lastName != null ? this.lastName.hashCode() : 0);
+        hash = 97 * hash + (this.address != null ? this.address.hashCode() : 0);
+        hash = 97 * hash + (this.phoneNumber != null ? this.phoneNumber.hashCode() : 0);
+        hash = 97 * hash + (this.password != null ? this.password.hashCode() : 0);
+        hash = 97 * hash + (this.active ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final PersonDTO other = (PersonDTO) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        if (this.orders != other.orders && (this.orders == null || !this.orders.equals(other.orders))) {
+            return false;
+        }
+        if ((this.firstName == null) ? (other.firstName != null) : !this.firstName.equals(other.firstName)) {
+            return false;
+        }
+        if ((this.lastName == null) ? (other.lastName != null) : !this.lastName.equals(other.lastName)) {
+            return false;
+        }
+        if ((this.address == null) ? (other.address != null) : !this.address.equals(other.address)) {
+            return false;
+        }
+        if ((this.phoneNumber == null) ? (other.phoneNumber != null) : !this.phoneNumber.equals(other.phoneNumber)) {
+            return false;
+        }
+        if ((this.password == null) ? (other.password != null) : !this.password.equals(other.password)) {
+            return false;
+        }
+        if (this.active != other.active) {
+            return false;
+        }
+        if (this.isServiceman != other.isServiceman) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+}
---
+++ trunk/tireservice/src/test/java/DTOTests/PersonDTOImplTests.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTOTests;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Martin
+ */
+public class PersonDTOImplTests {
+    
+    @Test
+    public void TODOTESTS(){
+       
+    }
+}
