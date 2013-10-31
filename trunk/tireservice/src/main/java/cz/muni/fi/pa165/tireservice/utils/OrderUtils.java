2f0947b8909efdce801da21f1c495d058cb1b2d2
jakub.papcun@gmail.com <jakub.papcun@gmail.com@fdf17263-572c-81d4-9246-cce687cf71e3>
2013-10-31 18:48:08 +0000

    JP | utils finalization

--- trunk/tireservice/src/main/java/cz/muni/fi/pa165/tireservice/utils/OrderUtils.java
+++ trunk/tireservice/src/main/java/cz/muni/fi/pa165/tireservice/utils/OrderUtils.java
@@ -1,7 +1,13 @@
 package cz.muni.fi.pa165.tireservice.utils;
 
 import cz.muni.fi.pa165.tireservice.dto.OrderDTO;
+import cz.muni.fi.pa165.tireservice.dto.ServiceDTO;
+import cz.muni.fi.pa165.tireservice.dto.TireDTO;
 import cz.muni.fi.pa165.tireservice.entities.Order;
+import cz.muni.fi.pa165.tireservice.entities.Service;
+import cz.muni.fi.pa165.tireservice.entities.Tire;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -19,10 +25,31 @@ public class OrderUtils {
         orderDTO.setCarType(order.getCarType());
         orderDTO.setDate(order.getDate());
         orderDTO.setId(order.getId());
-                
-        //orderDTO.setPerson(order.getPerson());
-        //orderDTO.setTires(order.getTires());
-        //orderDTO.setServices(order.getServices());
+        
+        if(order.getPerson() != null){
+            orderDTO.setPerson(PersonUtils.getPersonDTOFromEntity(order.getPerson()));
+        }
+        
+        List<Tire> listTires = order.getTires();
+        if(listTires != null && !listTires.isEmpty()){
+            List<TireDTO> listTiresDTO = new ArrayList<TireDTO>();
+            for(Tire tire : listTires){
+                TireDTO tireDTO = TireUtils.getTireDTOFromEntity(tire);
+                listTiresDTO.add(tireDTO);
+            }
+            orderDTO.setTires(listTiresDTO);
+        }
+        
+        List<Service> listServices = order.getServices();
+        if(listServices != null && !listServices.isEmpty()){
+            List<ServiceDTO> listServiceDTO = new ArrayList<ServiceDTO>();
+            for(Service service : listServices){
+                ServiceDTO serviceDTO = ServicesUtils.getServiceDTOFromEntity(service);
+                listServiceDTO.add(serviceDTO);
+            }
+            orderDTO.setServices(listServiceDTO);
+        }
+        
         return orderDTO;
         
     }
@@ -37,10 +64,31 @@ public class OrderUtils {
         order.setCarType(orderDTO.getCarType());
         order.setDate(orderDTO.getDate());
         //order.setId(orderDTO.getId());
-                
-        //orderDTO.setPerson(orderDTO.getPerson());
-        //orderDTO.setTires(orderDTO.getTires());
-        //orderDTO.setServices(orderDTO.getServices());
+        
+        if(orderDTO.getPerson() != null){
+            order.setPerson(PersonUtils.personDTOToEntity(orderDTO.getPerson()));
+        }
+        
+        List<TireDTO> listTiresDTO = orderDTO.getTires();
+        if(listTiresDTO != null && !listTiresDTO.isEmpty()){
+            List<Tire> listTires  = new ArrayList<Tire>();
+            for(TireDTO tireDTO : listTiresDTO){
+                Tire tire = TireUtils.tireDTOToEntity(tireDTO);
+                listTiresDTO.add(tireDTO);
+            }
+            order.setTires(listTires);
+        }
+        
+        List<ServiceDTO> listServiceDTO = orderDTO.getServices();
+        if(listServiceDTO != null && !listServiceDTO.isEmpty()){
+            List<Service> listServices = new ArrayList<Service>();
+            for(ServiceDTO serviceDTO : listServiceDTO){
+                Service service = ServicesUtils.serviceDTOToEntity(serviceDTO);
+                listServices.add(service);
+            }
+            order.setServices(listServices);
+        }
+        
         return order;
     }
     
