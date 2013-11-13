0b86dd41608c29efce4d149252972de849e29a4e
jakub.papcun@gmail.com <jakub.papcun@gmail.com@fdf17263-572c-81d4-9246-cce687cf71e3>
2013-11-13 19:00:54 +0000

    JP | tests moved to implementation + other changes

--- trunk/tireservice/tireservice-impl/src/test/java/cz/muni/fi/pa165/tireservice/daotest/AbstractDAOTest.java
+++ trunk/tireservice/tireservice-impl/src/test/java/cz/muni/fi/pa165/tireservice/daotest/AbstractDAOTest.java
@@ -5,19 +5,20 @@
 package cz.muni.fi.pa165.tireservice.daotest;
 
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
-
 /**
  *
  * @author Martin
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:applicationContext.xml", "classpath:/dataSource.xml"})
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 @TransactionConfiguration(defaultRollback = true)
+@ActiveProfiles(profiles = "test")
 @Transactional
 public abstract class AbstractDAOTest {
     
