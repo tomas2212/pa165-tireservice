c097f15d438e353fd357858990d54c7758eda0e9
stefan.sakala@gmail.com <stefan.sakala@gmail.com@fdf17263-572c-81d4-9246-cce687cf71e3>
2013-12-01 00:24:06 +0000

    Added CRUD methods in ServicesController and TireTypeController

--- trunk/tireservice/tireservice-web/src/main/java/cz/muni/fi/pa165/tireservice/rest/ServicesController.java
+++ trunk/tireservice/tireservice-web/src/main/java/cz/muni/fi/pa165/tireservice/rest/ServicesController.java
@@ -5,21 +5,57 @@ import cz.muni.fi.pa165.tireservice.services.ServiceServices;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("services")
 public class ServicesController {
-    
-        @Autowired
-	ServiceServices serviceServices;
 
-	@RequestMapping
-	@ResponseBody
-	public List<ServiceDTO> allServices() {
-            List<ServiceDTO> services = serviceServices.getAllEnabledServices();
-            return services;
-	}
-        
+    @Autowired
+    ServiceServices serviceServices;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public ServiceDTO get(@PathVariable("id") Long id) {
+        return serviceServices.getServiceById(id);
+    }
+
+    @RequestMapping
+    @ResponseBody
+    public List<ServiceDTO> allServices() {
+        List<ServiceDTO> services = serviceServices.getAllEnabledServices();
+        return services;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ResponseBody
+    public ServiceDTO createService(@RequestBody ServiceDTO serviceDTO) {
+        serviceServices.createService(serviceDTO);
+        return serviceDTO;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public ServiceDTO deleteService(@PathVariable("id") Long id) {
+        ServiceDTO serviceDTO = serviceServices.getServiceById(id);
+        if (serviceDTO != null) {
+            serviceServices.removeService(serviceDTO);
+        } else {
+            throw new IllegalArgumentException("Service with this id was not found");
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @ResponseBody
+    public ServiceDTO updateService(@PathVariable("id") Long id, @RequestBody ServiceDTO serviceDTO) {
+        serviceDTO.setId(id);
+        serviceServices.updateService(serviceDTO);
+
+        return serviceDTO;
+    }
 }
--- trunk/tireservice/tireservice-web/src/main/java/cz/muni/fi/pa165/tireservice/rest/TireTypeController.java
+++ trunk/tireservice/tireservice-web/src/main/java/cz/muni/fi/pa165/tireservice/rest/TireTypeController.java
@@ -9,7 +9,10 @@ import cz.muni.fi.pa165.tireservice.services.ServiceTireType;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -23,10 +26,44 @@ public class TireTypeController {
     @Autowired
     ServiceTireType tireTypeServices;
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public TireTypeDTO getTire(@PathVariable("id") Long id) {
+        return tireTypeServices.getTireTypeById(id);
+    }
+
     @RequestMapping
     @ResponseBody
     public List<TireTypeDTO> allTireTypes() {
         List<TireTypeDTO> tireTypes = tireTypeServices.getAllTireTypes();
         return tireTypes;
+    }
+    
+     @RequestMapping(value = "/", method = RequestMethod.POST)
+    @ResponseBody
+    public TireTypeDTO createTireType(@RequestBody TireTypeDTO tireTypeDTO) {
+        tireTypeServices.createTireType(tireTypeDTO);
+        return tireTypeDTO;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @ResponseBody
+    public TireTypeDTO deleteTireType(@PathVariable("id") Long id) {
+        TireTypeDTO tireTypeDTO = tireTypeServices.getTireTypeById(id);
+        if (tireTypeDTO != null) {
+            tireTypeServices.removeTireType(tireTypeDTO);
+        } else {
+            throw new IllegalArgumentException("Service with this id was not found");
+        }
+        return null;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @ResponseBody
+    public TireTypeDTO updateTireType(@PathVariable("id") Long id, @RequestBody TireTypeDTO tireTypeDTO) {
+        tireTypeDTO.setId(id);
+        tireTypeServices.updateTireType(tireTypeDTO);
+
+        return tireTypeDTO;
     }
 }
